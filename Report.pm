5f6958aaa28494b455480738a7970bded08a7d10
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-08-09 17:35:51 +0200

    Add first stub

---
+++ Report.pm
@@ -0,0 +1,22 @@
+package Data::MARC::Validator::Report;
+
+use strict;
+use warnings;
+
+use Mo qw(build is);
+
+our $VERSION = 0.01;
+
+#has type_of_date => (
+#	is => 'ro',
+#);
+
+sub BUILD {
+	my $self = shift;
+
+	return;
+}
+
+1;
+
+__END__
